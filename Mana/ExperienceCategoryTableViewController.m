a72a2d2b13f7d9e3128d9b39738973b272182785
Scott Shebby <scott@Scotts-MacBook-Pro.local>
2014-06-04 22:58:12 -0700

    view formatting changes

--- Mana/ExperienceCategoryTableViewController.m
+++ Mana/ExperienceCategoryTableViewController.m
@@ -92,7 +92,7 @@
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     if( indexPath.row == 0 ){
-        return 200.;
+        return 90.;
     }
     return 44.;
 }
